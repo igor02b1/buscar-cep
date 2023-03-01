62542bbf0574c4ba1e7b2fd39224a6cb165c4d33
igor02b1 <igor02b1@gmail.com>
2023-02-28 23:13:02 -0300

    feat: organizing folders

---
+++ script.js
@@ -0,0 +1,26 @@
+function consultaEndereco() {
+    let cep = document.querySelector('#cep').value;
+
+    if(cep ==! 8){
+        alert('CEP inválido');
+        return
+    }
+
+    let url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    fetch(url).then(function(response){
+        response.json().then(mostrarEndereco)
+    });
+}
+
+function mostrarEndereco(dados) {
+    let resultado = document.querySelector('#resultado');
+    if(dados.erro) {
+        resultado.innerHTML = "não foi possível localizar o endereço";
+    } else {
+    resultado.innerHTML = `<p>Endereço: ${dados.logradouro}</p>
+                            <p>Complemento: ${dados.complemento}</p>
+                            <p>Bairro: ${dados.bairro}</p>
+                            <p>Cidade: ${dados.localidade} - ${dados.bairro}</p>`
+            }
+}
